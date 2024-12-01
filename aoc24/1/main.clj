39e830b194940261b01a518188475cdb3fa25845
Riley Thompson <rileythompson99@gmail.com>
2024-12-01 01:10:43 -0500

    2024 1

--- aoc24/1/main.clj
+++ aoc24/1/main.clj
@@ -4,17 +4,6 @@
                  str/split-lines
                  (map #(str/split % #"   ")))
       lefts (sort (map #(Integer/parseInt (first %)) pairs))
-      rights (sort (map #(Integer/parseInt (second %)) pairs))
-      ans (->> (map #(abs (- %1 %2)) lefts rights)
-               (reduce +))]
-  (println ans))
-
-(let [pairs (->> (slurp "in.txt")
-                 str/split-lines
-                 (map #(str/split % #"   ")))
-      lefts (sort (map #(Integer/parseInt (first %)) pairs))
-      rights (sort (map #(Integer/parseInt (second %)) pairs))
-      ans (->> (map #(* % ((frequencies rights) % 0)) lefts)
-               (reduce +))]
-  (println ans))
-
+      rights (sort (map #(Integer/parseInt (second %)) pairs))]
+  (println (reduce + (map #(abs (- %1 %2)) lefts rights)))
+  (println (reduce + (map #(* % ((frequencies rights) % 0)) lefts))))
--- aoc24/1/main.py
+++ aoc24/1/main.py
@@ -3,26 +3,14 @@ import sys
 data = open(sys.argv[1]).read().strip()
 lines = data.split('\n')
 
-lefts = []
-rights = []
-
+lefts, rights = [], []
 for line in lines:
-    vals = line.split(' ')
-    left, right = int(vals[0]), int(vals[3])
-    lefts.append(left)
-    rights.append(right)
+    left, right = line.split('   ')
+    lefts.append(int(left))
+    rights.append(int(right))
 
 sorted_lefts = sorted(lefts)
 sorted_rights = sorted(rights)
 
-print(sum(abs(sorted_lefts[i] - sorted_rights[i]) for i in range(len(sorted_lefts))))
-
-ans = 0
-for sl in sorted_lefts:
-    sl_count = 0
-    for sr in sorted_rights:
-        if sl == sr:
-            sl_count += 1
-    ans += sl_count*sl
-
-print(ans)
+print(sum(abs(sl - sr) for sl, sr in zip(sorted_lefts, sorted_rights)))
+print(sum(sl * sorted_rights.count(sl) for sl in sorted_lefts))
