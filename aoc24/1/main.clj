4cf039950987c3380db7f4fa0a7a0c3fd50541dc
Riley Thompson <rileythompson99@gmail.com>
2024-12-02 01:33:45 -0500

    2024 1 clojure

--- aoc24/1/main.clj
+++ aoc24/1/main.clj
@@ -1,9 +1,11 @@
 (require '[clojure.string :as str])
 
-(let [pairs (->> (slurp "in.txt")
-                 str/split-lines
-                 (map #(str/split % #"   ")))
-      lefts (sort (map #(Integer/parseInt (first %)) pairs))
-      rights (sort (map #(Integer/parseInt (second %)) pairs))]
-  (println (reduce + (map #(abs (- %1 %2)) lefts rights)))
-  (println (reduce + (map #(* % ((frequencies rights) % 0)) lefts))))
+(def pairs (->> (slurp "in.txt")
+                str/split-lines
+                (map #(str/split % #"   "))))
+
+(def lefts (sort (map #(Integer/parseInt (first %)) pairs)))
+(def rights (sort (map #(Integer/parseInt (second %)) pairs)))
+
+(println (reduce + (map #(abs (- %1 %2)) lefts rights)))
+(println (reduce + (map #(* % ((frequencies rights) % 0)) lefts)))
