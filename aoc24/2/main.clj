73aa7fe9b72f8887f771a861511862d7665b905f
Riley Thompson <rileythompson99@gmail.com>
2024-12-02 01:22:26 -0500

    2024 2 clojure

--- aoc24/2/main.clj
+++ aoc24/2/main.clj
@@ -1,23 +1,14 @@
 (require '[clojure.string :as str])
 
-(defn ordered? [coll]
-  (or (= coll (sort coll))
-      (= coll (sort > coll))))
+(defn ordered? [nums]
+  (or (= nums (sort nums)) (= nums (sort > nums))))
 
 (defn adjacent-diff? [nums]
-  (every? #(and (>= % 1) (<= % 3)) (map #(abs (- %1 %2)) (rest nums) nums)))
+  (->> (map #(abs (- %1 %2)) (rest nums) nums)
+       (every? #(and (>= % 1) (<= % 3)))))
 
 (defn is-safe? [nums]
   (and (ordered? nums) (adjacent-diff? nums)))
-
-(def ans (->> (slurp "in.txt")
-              str/split-lines
-              (map (fn [line] (map #(Integer/parseInt %) (str/split line #" "))))
-              (map is-safe?)
-              (filter true?)
-              count))
-
-(println ans)
 
 (defn remove-each [nums]
   (map-indexed
@@ -25,14 +16,9 @@
      (concat (take idx nums) (drop (inc idx) nums)))
    nums))
 
-(defn is-safe2? [nums]
-  (some #(and (ordered? %) (adjacent-diff? %)) (remove-each nums)))
-
-(def ans2 (->> (slurp "in.txt")
+(def data (->> (slurp "in.txt")
                str/split-lines
-               (map (fn [line] (map #(Integer/parseInt %) (str/split line #" "))))
-               (map is-safe2?)
-               (filter true?)
-               count))
+               (map (fn [line] (map #(Integer/parseInt %) (str/split line #" "))))))
 
-(println ans2)
+(println (count (filter is-safe? data)))
+(println (count (filter #(some is-safe? (remove-each %)) data)))
