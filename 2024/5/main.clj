f9b4d4a01df34df3df3c6b35db63f374776cd770
rileythomp <rileythompson@flybits.com>
2024-12-05 15:14:14 -0500

    2024 5

--- 2024/5/main.clj
+++ 2024/5/main.clj
@@ -8,51 +8,65 @@
 
 (def followers (loop [orders orders
                       followers {}]
-                 (if (empty? orders) followers
+                 (if (empty? orders)
+                   followers
                    (let [[followed follower]
                          (mapv Integer/parseInt (str/split (first orders) #"\|"))]
                      (recur (rest orders)
                             (update followers followed (fnil conj []) follower))))))
 
-(def updates (->> (str/split (second chunks) #"\n") 
+(def updates (->> (str/split (second chunks) #"\n")
                   (mapv #(mapv Integer/parseInt (str/split % #",")))))
 
-(defn ordered? [nums] (loop [nums nums seen {}]
-                        (if (empty? nums) true
+(defn ordered? [nums] (loop [nums nums
+                             seen #{}]
+                        (if (empty? nums)
+                          true
                           (let [num (first nums)
                                 follows (get followers num [])]
-                            (if (some #(contains? seen %) follows) false
-                              (recur (rest nums) (assoc seen num true)))))))
+                            (if (some seen follows)
+                              false
+                              (recur (rest nums) (conj seen num)))))))
 
-(defn reorder [nums]
-  (if (ordered? nums) nums
-    (reorder (loop [i 0 nums nums seen {}]
-               (if (>= i (count nums)) nums
-                   (let [num (nth nums i)
-                         follows (get followers num [])
-                         seen (assoc seen num i)
-                         numseen (loop [follows follows seen seen nums nums]
-                                   (if (empty? follows)
-                                     [nums seen]
-                                     (let [follow (first follows)]
-                                       (if (and (contains? seen follow) (< (seen follow) (seen num)))
-                                         (let [num_at (seen num)
-                                               follow_at (seen follow)
-                                               nums_follow_at (nth nums follow_at)
-                                               nums_num_at (nth nums num_at)
-                                               nums' (-> nums
-                                                         (assoc num_at nums_follow_at)
-                                                         (assoc follow_at nums_num_at))
-             
-                                               seen' (-> seen
-                                                         (assoc follow num_at)
-                                                         (assoc num follow_at))]
-                                           (recur (rest follows) seen' nums'))
-                                         (recur (rest follows) seen nums)))))]
-                     (recur (inc i) (first numseen) (second numseen))))))))
-  
+(defn swap-nums [num follow nums seen]
+  (let [num_at (seen num)
+        follow_at (seen follow)]
+    [(-> nums
+         (assoc num_at (nth nums follow_at))
+         (assoc follow_at (nth nums num_at)))
+     (-> seen
+         (assoc follow num_at)
+         (assoc num follow_at))]))
+
+(defn order-num [num follows nums seen]
+  (reduce
+   (fn [[nums seen] follow]
+     (if (and (contains? seen follow)
+              (< (seen follow) (seen num)))
+       (swap-nums num follow nums seen)
+       [nums seen]))
+   [nums seen]
+   follows))
+
+(defn order-at-idx [idx nums seen]
+  (let [num (nth nums idx)
+        follows (get followers num [])
+        seen (assoc seen num idx)
+        [nums seen] (order-num num follows nums seen)]
+    [nums seen]))
+
+(defn order-nums [nums]
+  (if (ordered? nums)
+    nums
+    (let [[nums _]
+          (reduce
+           (fn [[nums seen] i] (order-at-idx i nums seen))
+           [nums {}]
+           (range (count nums)))]
+      (order-nums nums))))
+
 (def ans (->> updates
-              (mapv (fn [nums] 
+              (mapv (fn [nums]
                       (if (ordered? nums)
                         (nth nums (quot (count nums) 2))
                         0)))
@@ -61,7 +75,7 @@
 (def ans2 (->> updates
                (mapv  (fn [nums]
                         (if (not (ordered? nums))
-                          (nth (reorder nums) (quot (count nums) 2))
+                          (nth (order-nums nums) (quot (count nums) 2))
                           0)))
                (reduce +)))
 
