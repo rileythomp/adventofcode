807bdcfa8a7652b71e5cb0d14c78b8c5f6f33679
Riley Thompson <rileythompson99@gmail.com>
2024-12-08 13:11:42 -0500

    2024 8

--- 2024/8/main.clj
+++ 2024/8/main.clj
@@ -7,53 +7,51 @@
 
 (def grid (mapv (fn [line] (into [] line)) lines))
 
-(def freqs (reduce (fn [acc [y row]]
-                     (reduce (fn [acc [x cell]]
+(def freqs (reduce (fn [freqs [y row]]
+                     (reduce (fn [freqs [x cell]]
                                (if (not= cell \.)
-                                 (update acc cell (fnil conj []) [y x])
-                                 acc))
-                             acc
+                                 (update freqs cell (fnil conj []) [y x])
+                                 freqs))
+                             freqs
                              (map-indexed vector row)))
                    {}
                    (map-indexed vector grid)))
 
-(defn in-bounds? [y x]
+(defn in-bounds? [[y x]]
   (and (>= y 0) (< y (count grid))
        (>= x 0) (< x (count (grid 0)))))
 
-(defn add-obstacles [seen [oy1 ox1 oy2 ox2]]
+(defn add-obstacles [seen [p1 p2]]
   (cond-> seen
-    (in-bounds? oy1 ox1) (conj [oy1 ox1])
-    (in-bounds? oy2 ox2) (conj [oy2 ox2])))
+    (in-bounds? p1) (conj p1)
+    (in-bounds? p2) (conj p2)))
 
-(defn get-obstacles [y1 x1 y2 x2 dy dx]
-  [(- y1 dy) (- x1 dx) (+ y2 dy) (+ x2 dx)])
+(defn get-obstacles [[y1 x1] [y2 x2] dy dx]
+  [[(- y1 dy) (- x1 dx)] [(+ y2 dy) (+ x2 dx)]])
 
 (def ans (->> freqs
               (reduce-kv (fn [seen _ coords]
-                           (let [pairs (combo/combinations coords 2)]
-                             (reduce (fn [seen [[y1 x1] [y2 x2]]]
-                                       (add-obstacles seen
-                                                      (get-obstacles y1 x1 y2 x2 (- y2 y1) (- x2 x1))))
-                                     seen
-                                     pairs)))
+                           (reduce (fn [seen [[y1 x1 :as p1] [y2 x2 :as p2]]]
+                                     (add-obstacles seen
+                                                    (get-obstacles p1 p2 (- y2 y1) (- x2 x1))))
+                                   seen
+                                   (combo/combinations coords 2)))
                          #{})
               count))
 
 (def ans2 (->> freqs
                (reduce-kv (fn [seen _ coords]
-                            (let [pairs (combo/combinations coords 2)]
-                              (reduce (fn [seen [[y1 x1] [y2 x2]]]
-                                        (let [dy (- y2 y1) dx (- x2 x1)]
-                                          (loop [seen (conj seen [y1 x1] [y2 x2])
-                                                 [oy1 ox1 oy2 ox2] (get-obstacles y1 x1 y2 x2 dy dx)]
-                                            (if (or (in-bounds? oy1 ox1)
-                                                    (in-bounds? oy2 ox2))
-                                              (recur (add-obstacles seen [oy1 ox1 oy2 ox2])
-                                                     (get-obstacles oy1 ox1 oy2 ox2 dy dx))
-                                              seen))))
-                                      seen
-                                      pairs)))
+                            (reduce (fn [seen [[y1 x1 :as p1] [y2 x2 :as p2]]]
+                                      (let [dy (- y2 y1) dx (- x2 x1)]
+                                        (loop [seen (conj seen p1 p2)
+                                               [p1 p2] (get-obstacles p1 p2 dy dx)]
+                                          (if (or (in-bounds? p1)
+                                                  (in-bounds? p2))
+                                            (recur (add-obstacles seen [p1 p2])
+                                                   (get-obstacles p1 p2 dy dx))
+                                            seen))))
+                                    seen
+                                    (combo/combinations coords 2)))
                           #{})
                count))
 
